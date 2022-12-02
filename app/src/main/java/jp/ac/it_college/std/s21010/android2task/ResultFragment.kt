7f7deea9fd55fd80a5c5b72f4250c68df95cb557
s21010 <s21010@std.it-college.ac.jp>
2022-12-02 17:05:58 +0900

    取得(URL)

--- app/src/main/java/jp/ac/it_college/std/s21010/android2task/ResultFragment.kt
+++ app/src/main/java/jp/ac/it_college/std/s21010/android2task/ResultFragment.kt
@@ -23,7 +23,6 @@ class ResultFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
     }
 
